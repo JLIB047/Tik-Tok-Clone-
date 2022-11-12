16848ea4df6ba3c74bb5311fd473c5d2db0ac042
Jason Libertelli <jason.libertelli@gmail.com>
2022-11-12 10:06:00 -0500

    like button functionality/ post details page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <GoogleOAuthProvider clientId={ `${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}` } >
+      <div className="xl: w-1200px m-auto overflow-hidden h-[100vh]">
       <Navbar></Navbar>
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -28,7 +29,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <Component {...pageProps} />
         </div>
       </div>
-      
+    </div>  
     </GoogleOAuthProvider>  
   )
 }
