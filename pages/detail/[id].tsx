c63c76368c91cf98ee61ca68886f0a9e9aab58f1
Jason Libertelli <jason.libertelli@gmail.com>
2022-11-17 09:49:52 -0500

    comment functionality works - need to render on detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -24,8 +24,12 @@ const Detail = ({ postDetails }: IProps) => {
   const [post, setPost] = useState(postDetails); 
   const [ isVideoMuted, setIsVideoMuted ] = useState(false); 
   const [playing, setPlaying] = useState(false);
+  const  [ comment, setComment ] = useState(''); 
+  const [ isPostingComment, setIsPostingComment ] = useState(false);
+
   const videoRef = useRef<HTMLVideoElement>(null); 
   const router = useRouter();
+
   const { userProfile } : any = useAuthStore();
 
   const onVideoClick = () => {
@@ -55,6 +59,24 @@ const Detail = ({ postDetails }: IProps) => {
       setPost({ ...post, likes:response.data.likes });
     }
   }
+
+  const addComment = async (e: { preventDefault: () => void }) => {
+    e.preventDefault(); 
+
+    if(userProfile && comment) { 
+      setIsPostingComment(true);
+    
+
+    const { data } = await axios.put(`${BASE_URL}/api/post/${post._id}`, { 
+      userId: userProfile._id,
+      comment
+    });
+
+    setPost({ ...post, comments: data.comments }); 
+    setComment(''); 
+    setIsPostingComment(false);
+    }
+  };
 
   if(!post) return null; 
 
@@ -141,7 +163,13 @@ const Detail = ({ postDetails }: IProps) => {
                 /> 
               )}
               </div>    
-                <Comments />      
+                <Comments 
+                  comment={comment}
+                  setComment={setComment}
+                  addComment={addComment}
+                  comments={post.comments}
+                  isPostingComment={isPostingComment}
+                />      
           </div>
       </div>
     </div>
